4fd031ba3240efc9b6a50626873194d7e6bc8782
Sasank-Ss4 <99401760+Sasank-chhe@users.noreply.github.com>
2022-07-23 16:25:51 +0545

    Add files via upload

---
+++ sss.py
@@ -0,0 +1,14 @@
+import os, platform
+try:
+   import requests
+except:
+   os.system('pip install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from ss import mengecek_
+    mengecek_()
+elif bit == '32bit':
+    from ss import mengeck_
+    mengeck_()
